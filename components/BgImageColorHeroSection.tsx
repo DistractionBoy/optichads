e6cbc759690bcf729708b4a2458abc4ff613b195
Jason Pierce <jason.pierce.designs@gmail.com>
2022-06-26 22:40:16 -0400

    fix: update spacing in button box

--- components/BgImageColorHeroSection.tsx
+++ components/BgImageColorHeroSection.tsx
@@ -81,7 +81,7 @@ export default function BgImageColorHeroSection() {
               The Author
             </p>
             <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+              <div className="space-y-4 sm:space-y-0 sm:mx-auto">
                 <Link href="/mint" passHref>
                   <div className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8 cursor-pointer">
                     Mint a Chad
