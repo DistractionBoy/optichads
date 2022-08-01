9f47e81e4d812429ce03e5bf4862d2573398c45e
Jason Pierce <jason.pierce.designs@gmail.com>
2022-08-01 12:56:32 -0400

    feat: add new images and env vars

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -10,9 +10,6 @@ import {
   HeadMeta,
 } from "../components";
 import { MintFormContext } from "../lib/state/mintForm";
-import Image from "next/image";
-
-import vitalikApproves from "../public/images/vitalik_approves.png";
 
 const MintPage: NextPage = () => {
   const { state: formState } = useContext(MintFormContext);
@@ -26,20 +23,12 @@ const MintPage: NextPage = () => {
       />
       <DarkOverlapShell title="mint date not yet set">
         <div className="relative bg-white rounded-lg shadow">
-          {/* <Stepper />
+          <Stepper />
           {!formState.isReadyForStep2 && <MintStepOne />}
           {formState.isReadyForStep2 && !formState.isReadyForStep3 && (
             <MintStepTwo />
           )}
-          {formState.isReadyForStep3 && <MintStepThree />} */}
-          <Image
-            alt="a sick tweet bro"
-            className="rounded-lg"
-            src={vitalikApproves}
-            height={900}
-            width={1255}
-            layout="responsive"
-          />
+          {formState.isReadyForStep3 && <MintStepThree />}
         </div>
       </DarkOverlapShell>
     </>
