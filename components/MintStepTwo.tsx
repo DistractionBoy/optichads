29f3c40fd413efd814d95ef46d7467f759fd401c
Jason Pierce <jason.pierce.designs@gmail.com>
2022-08-04 00:13:29 -0400

    feat: add rampp mint button

--- components/MintStepTwo.tsx
+++ components/MintStepTwo.tsx
@@ -17,15 +17,39 @@ import Modal from "./Modal";
 import { parseBalance } from "../lib/utils";
 import { classNames } from "../lib/helpers";
 import { ethers } from "ethers";
+import { MerkleTree } from "merkletreejs";
+import Script from "next/script";
+import whitelist from "../lib/whitelist";
 import {
   TransactionReceipt,
   TransactionResponse,
 } from "@ethersproject/providers";
 import useETHBalance from "../lib/hooks/useEthBalance";
 
+const tempWhitelist = [
+  "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+  "0x3ED9d38601748734e94Ee8480077cc8D4C8ffb0C",
+];
+
+const padBuffer = (addr: string) => {
+  return Buffer.from(addr.substring(2).padStart(32 * 2, "0"), "hex");
+};
+
+const leaves = tempWhitelist.map((account) => padBuffer(account));
+const tree = new MerkleTree(leaves, ethers.utils.keccak256, { sort: true });
+
 const getPrice = async (contract: Contract) => {
   try {
     return await contract?.PRICE();
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
+};
+
+const getEarlyPrice = async (contract: Contract) => {
+  try {
+    return await contract?.EARLY_MINT_PRICE();
   } catch (e) {
     console.error(e);
     return e;
@@ -40,7 +64,7 @@ const getMintingOpen = async (contract: Contract) => {
   }
 };
 
-const onlyAllowListMode = async (contract: Contract) => {
+const onlyAllowlistMode = async (contract: Contract) => {
   try {
     return await contract?.onlyAllowlistMode();
   } catch (e) {
@@ -75,6 +99,31 @@ const getNextTokenId = async (contract: Contract) => {
     return e;
   }
 };
+
+const earlyMintOwnershipCap = async (contract: Contract) => {
+  try {
+    return await contract?.earlyMintOwnershipCap();
+  } catch (e) {
+    return e;
+  }
+};
+
+const getCanMintAmount = async (
+  contract: Contract,
+  address: string,
+  amount: number
+) => {
+  try {
+    return await contract?.canMintAmount(address, amount);
+  } catch (e) {
+    return e;
+  }
+};
+
+const getPriceAndByMode = async (
+  mode: "public" | "allowlist",
+  contract: Contract
+) => (mode === "public" ? await getPrice(contract) : getEarlyPrice(contract));
 
 // const postMsgToMintyBot = async (message: string) => {
 //   const msg = { content: message };
@@ -124,11 +173,17 @@ export default function MintStepTwo() {
   const [costPerToken, setCostPerToken] = useState<BigNumber>(
     BigNumber.from("150000000000000")
   );
-  const [isMintingAvailable, setIsMintingAvailable] = useState<boolean>(false);
+  const [isMintingAvailable, setIsMintingAvailable] = useState<
+    boolean | undefined
+  >(undefined);
+  const [isInOnlyAllowListMode, setIsInOnlyAllowListMode] =
+    useState<boolean>(false);
   const [welcomeMessage, setWelcomeMessage] = useState<string>();
   const [quantity, setQuantity] = useState<{ value: string }>({ value: "0" });
   const [isValid, setIsValid] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
+  const [canMintAmount, setCanMintAmount] = useState<boolean>(false);
+  const [maxNumToMint, setMaxNumToMint] = useState<number | undefined>();
   const [hasEnoughEth, setHasEnoughEth] = useState<boolean>(true);
   const { data: ethBal } = useETHBalance(account as string);
 
@@ -150,61 +205,92 @@ export default function MintStepTwo() {
   };
 
   useEffect(() => {
-    if (formState.contract && formState.pricePerUnit.eq(BigNumber.from("0"))) {
-      getMintingOpen(formState.contract).then((isAvailable: boolean) => {
+    if (formState.contract && account && isMintingAvailable === undefined) {
+      const chadContract = formState.contract as Contract;
+      getMintingOpen(chadContract).then((isAvailable: boolean) => {
         setIsMintingAvailable(isAvailable);
-        if (isAvailable) {
-          getPrice(formState.contract as Contract).then(
-            (cost: BigNumber) => {
-              if (cost) {
-                setCostPerToken(BigNumber.from(cost));
-                formDispatch({
-                  type: "setMintFormState",
-                  payload: {
-                    ...formState,
-                    pricePerUnit: BigNumber.from(cost),
-                  },
-                });
-              }
-            },
-            (error) => console.log(error)
-          );
-        } else {
-          setWelcomeMessage("Minting is not available. Check back soon!");
-        }
+        onlyAllowlistMode(chadContract).then((isInWhitelistMode) => {
+          setIsInOnlyAllowListMode(isInWhitelistMode);
+          setMaxNumToMint(isInWhitelistMode ? 2 : 5);
+          if (isAvailable) {
+            getPriceAndByMode(
+              isInWhitelistMode ? "allowlist" : "public",
+              chadContract
+            ).then(
+              (cost: BigNumber) => {
+                if (cost) {
+                  setCostPerToken(BigNumber.from(cost));
+                  formDispatch({
+                    type: "setMintFormState",
+                    payload: {
+                      ...formState,
+                      pricePerUnit: BigNumber.from(cost),
+                    },
+                  });
+                }
+              },
+              (error) => console.log(error)
+            );
+          } else {
+            setWelcomeMessage("Minting is not available. Check back soon!");
+          }
+        });
       });
     }
-  }, [formState, formDispatch]);
+  }, [
+    formState,
+    formDispatch,
+    isInOnlyAllowListMode,
+    account,
+    isMintingAvailable,
+  ]);
 
-  const handleSubmit = (e: FormEvent) => async (contract: Contract) => {
-    e.preventDefault();
-    const signer = provider && provider.getSigner(account);
-    const connectedContract = contract.connect(signer as ethers.Signer);
-    setLoading(true);
-    if (
-      account &&
-      hasEnoughEth &&
-      chainId === Number(process.env.NEXT_PUBLIC_CHAIN_ID)
-    ) {
-      const quan = BigNumber.from(quantity.value);
-      const total = costPerToken?.mul(quan);
-      try {
-        const response: TransactionResponse =
-          await connectedContract.mintToMultiple(quantity.value, {
-            to: account,
-            value: total,
-          });
-        const fullReceipt: TransactionReceipt = await response.wait();
-        return fullReceipt;
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      throw new Error(
-        "You need to switch to Optimism and/or make sure you have enough Eth"
-      );
-    }
-  };
+  // const handleSubmit = (e: FormEvent) => async (contract: Contract) => {
+  //   e.preventDefault();
+  //   const signer = provider && provider.getSigner(account);
+  //   const connectedContract = contract.connect(signer as ethers.Signer);
+  //   setLoading(true);
+  //   if (
+  //     account &&
+  //     hasEnoughEth &&
+  //     chainId === Number(process.env.NEXT_PUBLIC_CHAIN_ID)
+  //   ) {
+  //     const quan = BigNumber.from(quantity.value);
+  //     const total = costPerToken?.mul(quan);
+  //     if (isInOnlyAllowListMode) {
+  //       try {
+  //         const merkleProof = tree.getHexProof(padBuffer(account));
+  //         debugger;
+  //         const response: TransactionResponse =
+  //           await connectedContract.mintToMultipleAL(
+  //             account,
+  //             quantity.value,
+  //             merkleProof
+  //           );
+  //         const fullReceipt: TransactionReceipt = await response.wait();
+  //         return fullReceipt;
+  //       } catch (e) {
+  //         console.error(e);
+  //       }
+  //     } else {
+  //       try {
+  //         const response: TransactionResponse =
+  //           await connectedContract.mintToMultiple(quantity.value, {
+  //             to: account,
+  //             amount: total,
+  //           });
+  //         const fullReceipt: TransactionReceipt = await response.wait();
+  //         return fullReceipt;
+  //       } catch (e) {
+  //         console.log(e);
+  //       }
+  //     }
+  //   } else {
+  //     throw new Error(
+  //       "You need to switch to Optimism and/or make sure you have enough Eth"
+  //     );
+  //   }
+  // };
 
   return (
     <div className="bg-white overflow-hidden sm-rounded-b-lg pt-16">
@@ -228,106 +314,100 @@ export default function MintStepTwo() {
                     : "How many?"}
                 </span>
               </h1>
-              <form
-                className="flex flex-col justify-center"
-                onSubmit={(e) => {
-                  handleSubmit(e)(formState.contract as Contract)
-                    .then((receipt: TransactionReceipt | undefined) => {
-                      if (receipt) {
-                        formDispatch({
-                          type: "setReceipt",
-                          payload: receipt,
-                        });
-                        stepperDispatch({
-                          type: "setStepComplete",
-                          payload: 1,
-                        });
-                        // const txnLink = formatEtherscanLink(
-                        //   "Transaction",
-                        //   receipt.transactionHash
-                        // );
-                        getTotalMinted(formState.contract as Contract).then(
-                          (total: string) => {
-                            // updateRabbitHole(
-                            //   Number(quantity.value),
-                            //   txnLink,
-                            //   Number(total)
-                            // );
-                            formDispatch({
-                              type: "setStartingTokenId",
-                              payload: Number(total) - Number(quantity.value),
-                            });
-                          }
-                        );
-                        setTimeout(() => {
-                          stepperDispatch({
-                            type: "setCurrentStep",
-                            payload: 2,
-                          });
-                          formDispatch({
-                            type: "stepTwoComplete",
-                            payload: true,
-                          });
-                        }, 1250);
-                        setLoading(false);
-                      }
-                    })
-                    .catch((e) => alert(e));
-                }}
-              >
-                <div className="flex mt-12 justify-between">
-                  <label htmlFor="quantity" className="text-lg">
-                    Quantity (max: 3 per transaction)
-                  </label>
-                  <input
-                    name="quantity"
-                    value={quantity.value}
-                    type="range"
-                    min="0"
-                    max="3"
-                    step="1"
-                    onChange={numChadChangeHandler}
-                  />
+
+              <div className="flex mt-12 justify-between">
+                <label htmlFor="quantity" className="text-lg">
+                  Quantity (max: {`${maxNumToMint}`} per person)
+                </label>
+                <input
+                  name="quantity"
+                  value={quantity.value}
+                  type="range"
+                  min="0"
+                  max={maxNumToMint}
+                  step="1"
+                  onChange={numChadChangeHandler}
+                />
+              </div>
+              <div className="flex flex-col justify-center items-center text-7xl p-16">
+                {quantity.value}
+              </div>
+              <div className="flex justify-between items-center border-t-2 mt-2 px-2 py-2">
+                <div>Cost per Token:</div>
+                <div className="flex">
+                  <span className="mt-1 mr-2 h-3 w-3">
+                    <FontAwesomeIcon icon={faEthereum} />
+                  </span>{" "}
+                  {parseBalance(costPerToken?.toString())}
                 </div>
-                <div className="flex flex-col justify-center items-center text-7xl p-16">
-                  {quantity.value}
+              </div>
+              <div className="flex justify-between items-center border-t-2 border-b-2 mt-2 mb-8 px-2 py-2">
+                <div>Total Base Price: </div>
+                <div className="flex">
+                  <span className="mt-1 mr-2 h-3 w-3">
+                    <FontAwesomeIcon icon={faEthereum} />
+                  </span>{" "}
+                  {parseBalance(
+                    costPerToken?.mul(BigNumber.from(quantity.value)).toString()
+                  )}
                 </div>
-                <div className="flex justify-between items-center border-t-2 mt-2 px-2 py-2">
-                  <div>Cost per Token:</div>
-                  <div className="flex">
-                    <span className="mt-1 mr-2 h-3 w-3">
-                      <FontAwesomeIcon icon={faEthereum} />
-                    </span>{" "}
-                    {parseBalance(costPerToken?.toString())}
-                  </div>
+              </div>
+              <div className="flex justify-end">
+                <input
+                  className={classNames(
+                    isValid
+                      ? "cursor-pointer text-white bg-red-600 hover:bg-red-700 focus:ring-red-500"
+                      : "cursor-not-allowed text-red-600 bg-gray-50 hover:bg-gray-200 focus:ring-gray-100",
+                    " inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 "
+                  )}
+                  type="submit"
+                  value="Purchase"
+                  disabled={!isValid}
+                ></input>
+                <div
+                  id="rampp-minting-container-f6881fdb-33bf-44e5-b200-cba929c86e6d"
+                  className="rampp-minting-container"
+                >
+                  <button
+                    id="rampp-minting-button-f6881fdb-33bf-44e5-b200-cba929c86e6d"
+                    className="rampp-minting-button"
+                    style={{ display: "none" }}
+                    data-merkle-proof-uri="https://us-central1-nft-rampp.cloudfunctions.net/allowlist/N6ddW3Ynir3TTznZtOTM/merkle/verify"
+                    data-styles="eyJvcGVuIjp7InRleHQiOiJNaW50Iiwic3R5bGVzIjoiYm9yZGVyOm5vbmU7d2lkdGg6IDE1cmVtO3BhZGRpbmc6MC41cmVtO2ZvbnQtc2l6ZTogMS4xMjVyZW07bGluZS1oZWlnaHQ6IDEuNzVyZW07dGV4dC1hbGlnbjogY2VudGVyO2N1cnNvcjogcG9pbnRlcjtib3JkZXItcmFkaXVzOjAuMzc1cmVtO2NvbG9yOiNmZmZmZmY7YmFja2dyb3VuZC1jb2xvcjojYTgwMDAwOyJ9LCJwYXVzZWQiOnsidGV4dCI6Ik1pbnQlMjBDbG9zZWQiLCJzdHlsZXMiOiJib3JkZXI6bm9uZTt3aWR0aDogMTVyZW07cGFkZGluZzowLjVyZW07Zm9udC1zaXplOiAxLjEyNXJlbTtsaW5lLWhlaWdodDogMS43NXJlbTt0ZXh0LWFsaWduOiBjZW50ZXI7Y3Vyc29yOiBwb2ludGVyO2JvcmRlci1yYWRpdXM6MC4zNzVyZW07Y29sb3I6IzY3NjU2NTtiYWNrZ3JvdW5kLWNvbG9yOiNDQ0RCREM7Y3Vyc29yOm5vdC1hbGxvd2VkOyJ9LCJzdXBwbHlSZWFjaGVkIjp7InRleHQiOiJBbGwlMjBUb2tlbnMlMjBNaW50ZWQhIiwic3R5bGVzIjoiYm9yZGVyOm5vbmU7d2lkdGg6IDE1cmVtO3BhZGRpbmc6MC41cmVtO2ZvbnQtc2l6ZTogMS4xMjVyZW07bGluZS1oZWlnaHQ6IDEuNzVyZW07dGV4dC1hbGlnbjogY2VudGVyO2N1cnNvcjogcG9pbnRlcjtib3JkZXItcmFkaXVzOjAuMzc1cmVtO2NvbG9yOiMwMDAwMDA7YmFja2dyb3VuZC1jb2xvcjojMDBmZjJhO2N1cnNvcjpub3QtYWxsb3dlZDsifSwiZXJyb3IiOnsidGV4dCI6Ik1pbnRpbmcgVW5hdmFpbGFibGUiLCJzdHlsZXMiOiJib3JkZXI6bm9uZTt3aWR0aDogMTVyZW07cGFkZGluZzowLjVyZW07Zm9udC1zaXplOiAxLjEyNXJlbTtsaW5lLWhlaWdodDogMS43NXJlbTt0ZXh0LWFsaWduOiBjZW50ZXI7Y3Vyc29yOiBwb2ludGVyO2JvcmRlci1yYWRpdXM6MC4zNzVyZW07Y29sb3I6I2ZmMDAwMDtiYWNrZ3JvdW5kLWNvbG9yOiNmZmI4Yjg7Y3Vyc29yOm5vdC1hbGxvd2VkOyJ9LCJjbGFpbVRleHQiOnsidGV4dCI6bnVsbCwic3R5bGVzIjoiY29sb3I6IHJnYmEoMTU2LCAxNjMsIDE3NSk7Zm9udC1zaXplOiAwLjc1cmVtO2xpbmUtaGVpZ2h0OiAxcmVtO3RleHQtYWxpZ246IGNlbnRlcjtwYWRkaW5nLXRvcDogMC4yNXJlbTtwYWRkaW5nLWJvdHRvbTogMC4yNXJlbTttYXJnaW46MDtmb250LWZhbWlseTpzYW5zLXNlcmlmOyJ9LCJxdWFudGl0eSI6eyJ0ZXh0IjpudWxsLCJzdHlsZXMiOiJ3aWR0aDo0MHB4O2NvbG9yOiNhODAwMDA7Ym9yZGVyLXN0eWxlOnNvbGlkO2JvcmRlci13aWR0aDoxcHg7Ym9yZGVyLWNvbG9yOiNhODAwMDA7Ym9yZGVyLXJhZGl1czowLjM3NXJlbTtmb250LXNpemU6MS4zcmVtO3RleHQtYWxpZ246Y2VudGVyOyJ9fQ=="
+                    data-abi-link="https://firebasestorage.googleapis.com/v0/b/nft-rampp.appspot.com/o/solidity_outputs%2FN6ddW3Ynir3TTznZtOTM%2FOptiChadsContract_data-9870ab62-dd7d-451f-a40a-a576d1c969f1.json?alt=media"
+                    data-redirect="quixotic.com"
+                    data-contract-address="0x9B9F542456ad12796cCB8EB6644f29E3314e68e1"
+                    data-show-claim-count="false"
+                    data-network="optimism"
+                    data-format="multi"
+                    data-erc20-payments=""
+                    data-use-winter="false"
+                    data-winter-project-id="null"
+                  ></button>
                 </div>
-                <div className="flex justify-between items-center border-t-2 border-b-2 mt-2 mb-8 px-2 py-2">
-                  <div>Total Base Price: </div>
-                  <div className="flex">
-                    <span className="mt-1 mr-2 h-3 w-3">
-                      <FontAwesomeIcon icon={faEthereum} />
-                    </span>{" "}
-                    {parseBalance(
-                      costPerToken
-                        ?.mul(BigNumber.from(quantity.value))
-                        .toString()
-                    )}
-                  </div>
-                </div>
-                <div className="flex justify-end">
-                  <input
-                    className={classNames(
-                      isValid
-                        ? "cursor-pointer text-white bg-red-600 hover:bg-red-700 focus:ring-red-500"
-                        : "cursor-not-allowed text-red-600 bg-gray-50 hover:bg-gray-200 focus:ring-gray-100",
-                      " inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                    )}
-                    type="submit"
-                    value="Purchase"
-                    disabled={!isValid}
-                  ></input>
-                </div>
-              </form>
+                <Script
+                  src="https://cdnjs.cloudflare.com/ajax/libs/web3/1.7.0-rc.0/web3.min.js"
+                  crossOrigin="anonymous"
+                  referrerPolicy="no-referrer"
+                ></Script>
+                <Script
+                  type="text/javascript"
+                  src="https://unpkg.com/web3modal@1.9.8/dist/index.js"
+                ></Script>
+                <Script
+                  type="text/javascript"
+                  src="https://unpkg.com/evm-chains@0.2.0/dist/umd/index.min.js"
+                ></Script>
+                <Script
+                  type="text/javascript"
+                  src="https://unpkg.com/@walletconnect/web3-provider@1.7.8/dist/umd/index.min.js"
+                ></Script>
+                <Script
+                  type="text/javascript"
+                  src="https://rampp.xyz/embeds/v2.1/embed.js"
+                  data-uuid="f6881fdb-33bf-44e5-b200-cba929c86e6d"
+                ></Script>
+              </div>
             </div>
           ) : (
             <div className="text-lg max-w-prose mx-auto">
