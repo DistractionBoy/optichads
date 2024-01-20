22c324bbbc004404f328ec477e93ccd5a6b15653
DistractionBoy <jason.pierce.designs@gmail.com>
2024-01-20 16:38:52 -0500

    feat: add priority

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,48 +34,56 @@ const Home: NextPage = () => {
         <RotatingTitle />
         <div className="inset-0">
           <Image
+            priority
             alt=""
             src={mobile360}
             fill
             className="block xxs:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={mobile375}
             fill
             className="hidden xxs:block xs:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={mobile430}
             fill
             className="hidden xs:block sm:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={tablet}
             fill
             className="hidden sm:block lg:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={desktop}
             fill
             className="hidden lg:block ultrawide:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={ultrawide}
             fill
             className="hidden ultrawide:block verylarge:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={veryLarge}
             fill
             className="hidden verylarge:block superUltrawide:hidden object-cover"
           />
           <Image
+            priority
             alt=""
             src={superUltrawide}
             fill
