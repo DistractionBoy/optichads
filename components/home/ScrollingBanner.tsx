377c156187fb7eace40f030b5c69a6246857eb9c
DistractionBoy <jason.pierce.designs@gmail.com>
2024-01-21 13:20:35 -0500

    feat: componentize banner

---
+++ components/home/ScrollingBanner.tsx
@@ -0,0 +1,26 @@
+import banner from "@/public/images/banner.jpeg";
+import Image from "next/image";
+
+const wrapperClass =
+  "w-full inline-flex flex-nowrap overflow-hidden" +
+  "[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]";
+
+const ScrollingBanner = () => (
+  <div className={wrapperClass}>
+    <ul className="flex items-center justify-center md:justify-start [&_img]:max-w-none animate-infinite-scroll">
+      <li>
+        <Image priority src={banner} alt="" />
+      </li>
+    </ul>
+    <ul
+      aria-hidden
+      className="flex items-center justify-center md:justify-start [&_img]:max-w-none animate-infinite-scroll"
+    >
+      <li>
+        <Image src={banner} alt="" />
+      </li>
+    </ul>
+  </div>
+);
+
+export default ScrollingBanner;
--- pages/home.tsx
+++ pages/home.tsx
@@ -5,8 +5,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import HeadMeta from "@/components/HeadMeta";
 import DarkNavbar from "@/components/Navbar";
 import Team from "@/components/Team";
-import banner from "@/public/images/banner.jpeg";
-import Image from "next/image";
+import ScrollingBanner from "@/components/home/ScrollingBanner";
 
 export async function getStaticProps({ locale }: any) {
   return {
@@ -22,27 +21,7 @@ const Home: NextPage = () => {
     <>
       <HeadMeta />
       <DarkNavbar />
-      <div
-        x-data="{}"
-        x-init="$nextTick(() => {
-                        let ul = $refs.logos;
-                        ul.insertAdjacentHTML('afterend', ul.outerHTML);
-                        ul.nextSibling.setAttribute('aria-hidden', 'true');
-                    })"
-        className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
-      >
-        <ul
-          x-ref="logos"
-          className="flex items-center justify-center md:justify-start [&_img]:max-w-none animate-infinite-scroll"
-        >
-          <li>
-            <Image src={banner} alt="" />
-          </li>
-          <li>
-            <Image src={banner} alt="" />
-          </li>
-        </ul>
-      </div>
+      <ScrollingBanner />
 
       {/* <Team /> */}
       <div className="flex flex-col h-screen items-center justify-center">
