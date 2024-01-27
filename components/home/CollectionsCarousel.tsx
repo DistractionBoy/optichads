3d3c17cce4298c6c2101821410a7ebb9a7f17272
DistractionBoy <jason.pierce.designs@gmail.com>
2024-01-27 16:55:36 -0500

    feat: mobile styles updated

--- components/home/CollectionsCarousel.tsx
+++ components/home/CollectionsCarousel.tsx
@@ -11,15 +11,17 @@ import brigadeBanner from "@/public/images/banners/brigade-banner.jpeg";
 import { cn } from "@/lib/utils";
 
 const mobileCarouselItemCSS = cn(
-  "overflow-hidden pl-4 basis-1/3 rotate-3 [&>div]:h-[175px] [&_img]:mt-24 [&_img]:-rotate-3",
-  "[mask-image:_linear-gradient(to_bottom,transparent_0,_black_128px,_black_calc(100%-256px),transparent_100%)]"
+  "overflow-hidden pl-4 basis-1/3 rotate-3 [&>div]:h-[180px] [&_img]:mt-32 [&_img]:-rotate-3",
+  "[mask-image:_linear-gradient(to_bottom,transparent_0,_black_128px,_black_calc(100%-256px),transparent_100%)]",
+  "[&_ul]:animate-none [&_ul]:hover:animate-infinite-scroll",
+  "grayscale hover:grayscale-0 hover:cursor-pointer"
 );
 
 const CollectionsCarousel = () => (
   <>
     <div className="hidden xl:block">
       <Carousel className="border-black border-2">
-        <CarouselContent className="flex -ml-4 max-h-[600px] xl:max-h-[768px] desktop:max-h-[1080px] overflow-hidden -mt-20 -mb-8">
+        <CarouselContent className="flex -ml-4 h-[768px] desktop:h-[1080px] overflow-hidden -mt-20 -mb-8">
           <CarouselItem className="overflow-hidden pl-4 basis-1/3 rotate-3  [&_img]:-rotate-3">
             <ScrollingBanner src={chadBanner} />
           </CarouselItem>
@@ -35,7 +37,7 @@ const CollectionsCarousel = () => (
 
     <div className="block xl:hidden">
       <Carousel className="border-black border-2" orientation="vertical">
-        <CarouselContent className="flex flex-col -ml-4 overflow-hidden -mt-8 -mb-4">
+        <CarouselContent className="flex flex-col -ml-4 overflow-hidden -mt-8 -mb-4 h-[578px]">
           <CarouselItem className={mobileCarouselItemCSS}>
             <ScrollingBanner src={chadBanner} />
           </CarouselItem>
