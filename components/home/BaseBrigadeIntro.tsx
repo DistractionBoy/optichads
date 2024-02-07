be8178f339ab946d874ac901c06be7aaa0e0ca59
DistractionBoy <jason.pierce.designs@gmail.com>
2024-02-07 01:20:14 -0500

    feat: update animation loading3

--- components/home/BaseBrigadeIntro.tsx
+++ components/home/BaseBrigadeIntro.tsx
@@ -12,8 +12,9 @@ const BaseBrigadeIntro = () => {
   };
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-end items-stretch inset-0 bg-[#0052FE] border border-b-8 border-b-white">
-        <div className="flex flex-col items-center justify-center space-y-12 md:space-y-28 my-20 mx-20">
+      <div className="block relative w-full h-0 lg:h-44 desktop:h-64 bg-[#0052FE]" />
+      <div className="flex flex-col lg:flex-row justify-end xl:justify-center items-stretch bg-[#0052FE] border-b-8 border-b-white">
+        <div className="flex flex-col items-center justify-center space-y-12 md:space-y-28 my-20 px-20">
           <h2 className="text-3xl sm:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             GLAD <span className="inline-block lg:hidden">UR</span>
           </h2>
@@ -24,37 +25,33 @@ const BaseBrigadeIntro = () => {
             BASED
           </h2>
         </div>
-        <div className="flex justify-center lg:hidden">
+        <div className="flex justify-center xl:hidden">
           <Image
             alt="a cute soldier with a bag on his head, blinking every so often"
-            src={brigadimation}
-            width={700}
-            height={700}
+            src={imageSrc}
+            width={500}
+            height={500}
+            onLoad={onLoadingComplete}
           />
         </div>
 
-        <div className="hidden lg:flex desktop:hidden">
+        <div className="hidden xl:flex min-w-[700px]">
           <Image
             alt="a cute soldier with a bag on his head, blinking every so often"
-            src={brigadimation}
-            width={900}
-            height={900}
-          />
-        </div>
-
-        <div className="hidden desktop:flex">
-          <Image
-            alt="a cute soldier with a bag on his head, blinking every so often"
-            src={brigadimation}
+            src={imageSrc}
             width={1024}
             height={1024}
+            onLoad={onLoadingComplete}
           />
         </div>
-        <div className="flex lg:hidden flex-col items-center justify-center space-y-12 md:space-y-28 my-20 mb-32">
-          <h2 className="md:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
-            LIKE OUR
+        <div className="flex lg:hidden desktop:flex flex-col items-center justify-center space-y-12 md:space-y-28 my-20 mb-32 pr-20">
+          <h2 className="text-3xl sm:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+            LIKE <span className="inline-block lg:hidden">OUR</span>
           </h2>
-          <h2 className="md:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="hidden lg:flex text-3xl sm:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+            OUR
+          </h2>
+          <h2 className="text-3xl sm:text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             BRIGADE
           </h2>
         </div>
