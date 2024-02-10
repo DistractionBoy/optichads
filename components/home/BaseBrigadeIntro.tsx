c68037bf1819e29d15c275ced2d83f615c2fb034
DistractionBoy <jason.pierce.designs@gmail.com>
2024-02-10 17:48:54 -0500

    feat: update carousel to only show for desktop and up

--- components/home/BaseBrigadeIntro.tsx
+++ components/home/BaseBrigadeIntro.tsx
@@ -15,17 +15,17 @@ const BaseBrigadeIntro = () => {
       <div className="block relative w-full h-0 lg:h-44 desktop:h-64 bg-[#0052FE]" />
       <div className="flex flex-col lg:flex-row justify-end lg:justify-center items-stretch bg-[#0052FE] border-b-8 border-b-white">
         <div className="flex flex-col items-center justify-center space-y-12 md:space-y-28 my-20 px-20 xl:px-8">
-          <h2 className="text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             GLAD <span className="inline-block lg:hidden">UR</span>
           </h2>
-          <h2 className="hidden lg:flex text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="hidden lg:flex text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             UR
           </h2>
-          <h2 className="text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             BASED
           </h2>
         </div>
-        <div className="flex justify-center xl:hidden min-w-[500px]">
+        <div className="flex justify-center xl:hidden min-w-[390px]">
           <Image
             className="object-cover"
             alt="a cute soldier with a bag on his head, blinking every so often"
@@ -46,13 +46,13 @@ const BaseBrigadeIntro = () => {
           />
         </div>
         <div className="flex lg:hidden desktop:flex flex-col items-center justify-center space-y-12 md:space-y-28 my-20 mb-32 px-20 xl:px-8">
-          <h2 className="text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             LIKE <span className="inline-block lg:hidden">OUR</span>
           </h2>
-          <h2 className="hidden lg:flex text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="hidden lg:flex text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             OUR
           </h2>
-          <h2 className="text-5xl md:text-7xl xl:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
+          <h2 className="text-5xl md:text-7xl desktop:text-[96px] ultrawide:text-[120px] font-bold xl:font-black leading-8 font-outline-none md:font-outline-2 xl:font-outline-4">
             BRIGADE
           </h2>
         </div>
