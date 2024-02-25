86825d8794c3137bcdaad4389f126699b2766c32
DistractionBoy <jason.pierce.designs@gmail.com>
2024-02-25 15:04:16 -0500

    feat: voting section styling updates3

--- components/home/WeNeedYouChad.tsx
+++ components/home/WeNeedYouChad.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { divergentLinkButtonCSS } from "../ui/button";
 
 const WeNeedYouChad = () => (
-  <div className="relative rounded-xl mx-6 my-12 md:mx-8 md:my-16 lg:my-32 md:h-96 lg:h-[450px] bg-white/90 shadow-2xl drop-shadow-lg">
-    <div className="relative h-80 overflow-hidden rounded-l-xl bg-red-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+  <div className="relative rounded-xl mx-6 my-12 md:mx-8 md:my-16 lg:my-32 bg-white/90 shadow-2xl drop-shadow-lg">
+    <div className="relative overflow-hidden rounded-t-xl md:rounded-l-xl md:rounded-r-none bg-red-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
       <Image
         className="h-full w-full object-cover"
         src={weNeedYouImgSrc}
@@ -37,8 +37,8 @@ const WeNeedYouChad = () => (
         </defs>
       </svg>
     </div>
-    <div className="flex flex-col justify-center relative mx-auto max-w-7xl h-96 md:max-h-96 lg:px-8">
-      <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-12 lg:w-1/2 lg:pl-16 lg:pr-0 xl:pl-20 md:mt-8 lg:mt-20">
+    <div className="flex flex-col justify-center relative mx-auto max-w-7xl lg:px-8">
+      <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-12 lg:w-1/2 lg:pl-16 lg:pr-0 xl:pl-20 md:mt-8 my-8 md:my-4 lg:my-32">
         <h2 className="text-base font-semibold leading-7 text-red-600">
           We Need YOU!
         </h2>
