531a93ecba818d94b370c8d805c1587e70d46630
anh.nt4288@gmail.com <anh.nt4288@gmail.com>
2024-02-26 15:12:46 +0700

    add carousel item

--- components/collections/CollectionsCarouselItemController.tsx
+++ components/collections/CollectionsCarouselItemController.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import CarouselThumbs from "../ui/carouselThumbs";
+import { EmblaOptionsType } from 'embla-carousel'
+
+const OPTIONS: EmblaOptionsType = {}
+const SLIDE_COUNT = 6
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 const CollectionsCarouselItemController = () => {
 
   return (
     <>
-      <div className="block relative w-full h-96 bg-[#FB0420]" />
+      <div className="block relative w-full h-96 bg-[#FB0420]">
+        <CarouselThumbs slides={SLIDES} options={OPTIONS} />
+      </div>
     </>
   );
 };
---
+++ components/ui/carouselThumbs.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+type ThumbPropType = {
+  selected: boolean
+  index: number
+  onClick: () => void
+}
+
+const Thumb: React.FC<ThumbPropType> = (props) => {
+  const { selected, index, onClick } = props
+
+  return (
+    <div
+      className={'embla-thumbs__slide'.concat(
+        selected ? ' embla-thumbs__slide--selected' : ''
+      )}
+    >
+      <button
+        onClick={onClick}
+        type="button"
+        className="embla-thumbs__slide__number"
+      >
+        {index + 1}
+      </button>
+    </div>
+  )
+}
+
+const CarouselThumbs: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options)
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
+    containScroll: 'keepSnaps',
+    dragFree: true
+  })
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaMainApi || !emblaThumbsApi) return
+      emblaMainApi.scrollTo(index)
+    },
+    [emblaMainApi, emblaThumbsApi]
+  )
+
+  const onSelect = useCallback(() => {
+    if (!emblaMainApi || !emblaThumbsApi) return
+    setSelectedIndex(emblaMainApi.selectedScrollSnap())
+    emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap())
+  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex])
+
+  useEffect(() => {
+    if (!emblaMainApi) return
+    onSelect()
+    emblaMainApi.on('select', onSelect)
+    emblaMainApi.on('reInit', onSelect)
+  }, [emblaMainApi, onSelect])
+
+  return (
+    <div className="m-auto max-full embla">
+      <div className="embla__viewport" ref={emblaMainRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla-thumbs">
+        <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+          <div className="embla-thumbs__container">
+            {slides.map((index) => (
+              <Thumb
+                key={index}
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CarouselThumbs
