0cd6812d0e6efb5d5b46272651793f7b33efca0e
DistractionBoy <jason.pierce.designs@gmail.com>
2024-03-15 11:19:42 -0400

    feat: fix type error

--- components/opc/ClaimWithProof.tsx
+++ components/opc/ClaimWithProof.tsx
@@ -67,43 +67,28 @@ const ClaimWithProof = () => {
     );
   }
 
-  return data && data.length > 0 ? (
-    <Table>
-      <TableCaption>
-        Hop on the scale, bro. Let&apos;s see how shredded you are going to get!
-      </TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Address</TableHead>
+  return (
+    data && (
+      <Table>
+        <TableCaption>
+          Hop on the scale, bro. Let&apos;s see how shredded you are going to
+          get!
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Address</TableHead>
 
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>{data[0].address}</TableCell>
-          <TableCell className="text-right">{data[0].amount} $OPC</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  ) : (
-    <Table>
-      <TableCaption>
-        Sorry bro, looks like you are not on the list.
-      </TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Address</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>{address}</TableCell>
-          <TableCell className="text-right">0 $OPC</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>{data.address}</TableCell>
+            <TableCell className="text-right">{data.amount} $OPC</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    )
   );
 };
 
