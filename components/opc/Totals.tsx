d2bfb69025f83ed64d67317dade620457bc1341c
DistractionBoy <jason.pierce.designs@gmail.com>
2024-03-17 00:48:57 -0400

    feat: update positioning of consts

--- components/opc/Totals.tsx
+++ components/opc/Totals.tsx
@@ -13,8 +13,72 @@ import {
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useAccount } from "wagmi";
-import { toast } from "sonner";
 import { shortenHex } from "@/lib/utils";
+
+/**
+ * The following lines are the formulas used to produce the totals in the spreadsheet we made
+ * OptiChads!A:A;ArbiBabes!A:A;BaseBrigade!A:A;OptiChadsSong!A:A;ArbiBabesSong!A:A;
+ * EarlyOptimist!A:A;MetaBus!A:A;TickledPicklez!A:A;BoredTown!A:A;Apetimism!A:A;
+ * CryptoTesters!A:A;Motorheadz!A:A;OPBunnies!A:A;PoopNation!A:A;OPOrcas!A:A;
+ * BaseGods!A:A;Mochimons!A:A;BasedFellas!A:A;NFToshi!A:A;CollectiveNouns!A:A;
+ * DogePixels!A:A
+ *
+ * =SUM(Multiply(B1,14000),Multiply(C1,1400),Multiply(D1,1800),MIN(Multiply(E1,420), 10800),Min(Multiply(F1,420), 4200),
+ * Min(Multiply(G1, 420), 4200),Min(Multiply(H1,4200), 4200),Min(Multiply(I1,420), 4200),Min(Multiply(J1,4200), 42000),Min(Multiply(K1,420), 4200),
+ * Min(Multiply(L1,420), 4200),Min(Multiply(M1,4200), 84000),Min(Multiply(N1,4200), 42000),Min(Multiply(O1,420), 4200),Min(Multiply(P1,420), 4200),
+ * Min(Multiply(Q1,420), 4200),Min(Multiply(R1,420), 4200),Min(Multiply(S1,420), 4200),Min(Multiply(T1,420), 4200),Min(Multiply(U1,420), 4200),
+ * Min(Multiply(V1,420), 4200))
+ */
+
+const legend = [
+  (n: number) => n * 14000, //  "OptiChads",
+  (n: number) => n * 1400, //   "ArbiBabes",
+  (n: number) => n * 1800, //  "Base Brigade",
+  (n: number) => (n * 420 >= 10800 ? 10800 : n * 420), //  "OptiChads Song",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //  "ArbiBabes Song",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Early Optimist",
+  (n: number) => (n * 4200 >= 4200 ? 4200 : n * 4200), //   "MetaBus",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), // TickledPicklez
+  (n: number) => (n * 4200 >= 42000 ? 42000 : n * 4200), //   "Bored Town",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Apetimism",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //  "Crypto Testers",
+  (n: number) => (n * 4200 >= 84000 ? 84000 : n * 4200), //   "Motorheadz",
+  (n: number) => (n * 420 >= 42000 ? 42000 : n * 4200), //   "OP Bunnies",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Poop Nation",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "OP Orcas",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Base Gods",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Mochimons",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Based Fellas",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "NFToshi",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "Collective Nouns",
+  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420), //   "DogePixels",
+  (n: number) => n, // "Total",
+];
+
+const communities = [
+  "OptiChads",
+  "ArbiBabes",
+  "Base Brigade",
+  "OptiChads Song",
+  "ArbiBabes Song",
+  "Early Optimist",
+  "MetaBus",
+  "TickledPicklez",
+  "Bored Town",
+  "Apetimism",
+  "Crypto Testers",
+  "Motorheadz",
+  "OP Bunnies",
+  "Poop Nation",
+  "OP Orcas",
+  "Base Gods",
+  "Mochimons",
+  "Based Fellas",
+  "NFToshi",
+  "Collective Nouns",
+  "DogePixels",
+  "Total",
+];
 
 const Totals = () => {
   const { address } = useAccount();
@@ -77,71 +141,3 @@ const Totals = () => {
 };
 
 export default Totals;
-
-const legend = [
-  (n: number) => n * 14000,
-  (n: number) => n * 1400,
-  (n: number) => n * 1800,
-  (n: number) => (n * 420 >= 10800 ? 10800 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  // 2nd set
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 4200 >= 4200 ? 4200 : n * 4200),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 4200 >= 42000 ? 42000 : n * 4200),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  // 3rd
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 4200 >= 84000 ? 84000 : n * 4200),
-  (n: number) => (n * 420 >= 42000 ? 42000 : n * 4200),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  // 4th
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  // 5th
-  (n: number) => (n * 420 >= 4200 ? 4200 : n * 420),
-  (n: number) => n,
-];
-
-const communities = [
-  "OptiChads",
-  "ArbiBabes",
-  "Base Brigade",
-  "OptiChads Song",
-  "ArbiBabes Song",
-  "Early Optimist",
-  "MetaBus",
-  "TickledPicklez",
-  "Bored Town",
-  "Apetimism",
-  "Crypto Testers",
-  "Motorheadz",
-  "OP Bunnies",
-  "Poop Nation",
-  "OP Orcas",
-  "Base Gods",
-  "Mochimons",
-  "Based Fellas",
-  "NFToshi",
-  "Collective Nouns",
-  "DogePixels",
-  "Total",
-];
-
-/**
- * OptiChads!A:A;ArbiBabes!A:A;BaseBrigade!A:A;OptiChadsSong!A:A;ArbiBabesSong!A:A;
- * EarlyOptimist!A:A;MetaBus!A:A;TickledPicklez!A:A;BoredTown!A:A;Apetimism!A:A;
- * CryptoTesters!A:A;Motorheadz!A:A;OPBunnies!A:A;PoopNation!A:A;OPOrcas!A:A;
- * BaseGods!A:A;Mochimons!A:A;BasedFellas!A:A;NFToshi!A:A;CollectiveNouns!A:A;
- * DogePixels!A:A
- *
- * =SUM(Multiply(B1,14000),Multiply(C1,1400),Multiply(D1,1800),MIN(Multiply(E1,420), 10800),Min(Multiply(F1,420), 4200),
- * Min(Multiply(G1, 420), 4200),Min(Multiply(H1,4200), 4200),Min(Multiply(I1,420), 4200),Min(Multiply(J1,4200), 42000),Min(Multiply(K1,420), 4200),
- * Min(Multiply(L1,420), 4200),Min(Multiply(M1,4200), 84000),Min(Multiply(N1,4200), 42000),Min(Multiply(O1,420), 4200),Min(Multiply(P1,420), 4200),
- * Min(Multiply(Q1,420), 4200),Min(Multiply(R1,420), 4200),Min(Multiply(S1,420), 4200),Min(Multiply(T1,420), 4200),Min(Multiply(U1,420), 4200),
- * Min(Multiply(V1,420), 4200))
- */
